cace25f360702f1e718a6119d94ca5538ddfffad
Marc-07 <maanraca01@gmail.com>
2023-08-03 16:45:47 -0500

    Se agrega router y habilita pug

--- index.js
+++ index.js
@@ -1,4 +1,7 @@
-const express = require ('express');
+// const express = require ('express');
+import express from 'express';
+import router from './routes/index.js';
+
 
 const app = express();
 
@@ -6,6 +9,12 @@ const app = express();
 // Definir Puerto -- La variable de entorno aún no existe --
 const port = process.env.PORT || 4000;
 
+//Habilitar PUG
+app.set('view engine', 'pug');
+
+//Agregar router
+app.use('/', router);
+
 app.listen(port, ()=>{
     console.log(`El servidor está funcionando en el puerto ${port}`);
-})
+});
