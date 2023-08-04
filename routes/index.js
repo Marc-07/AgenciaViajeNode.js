ebefa1a3d583e7e9bd2a1f5c95c30dd8955cc648
Marc-07 <maanraca01@gmail.com>
2023-08-04 11:20:33 -0500

    Se agrega el archivo routs al proyecto

--- routes/index.js
+++ routes/index.js
@@ -3,13 +3,27 @@ import express from 'express';
 const router = express.Router();
 
 router.get('/', (req, res) => { //req - lo que enviamos // res - lo que express nos muestra
-    res.send('Inicio');
+    res.render('inicio', {
+        pagina: 'Inicio',
+    });
 });
 
-router.get('/Nosotros', (req, res) => { //req - lo que enviamos // res - lo que express nos muestra
-    res.send('Nosotros');
+router.get('/Nosotros', (req, res) => { 
+    res.render('nosotros',{
+        pagina: 'Nosotros'
+    });
 });
 
-router.get('/Contacto', (req, res) => { //req - lo que enviamos // res - lo que express nos muestra
-    res.send('Contacto');
+router.get('/Viajes', (req, res) => { 
+    res.render('viajes',{
+        pagina: 'Viajes'
+    });
 });
+
+router.get('/Testimoniales', (req, res) => { 
+    res.render('testimonio',{
+        pagina: 'Testimonios'
+    });
+});
+
+export default router;
